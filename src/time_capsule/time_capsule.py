7e9c4394336223d91526a823ad82776b2634366c
Marvin Gordon <1299703+FireSage@users.noreply.github.com>
2022-07-11 09:34:53 -0500

    Initial commit

---
+++ src/time_capsule/time_capsule.py
@@ -0,0 +1,19 @@
+import subprocess
+
+
+def run(command):
+	result = subprocess.run(["powershell", "-Command", command], capture_output=True,text=True)
+	return result
+
+
+processes = run("(Get-Process | Select MainWindowTitle,ProcessName,Id | where{$_.MainWindowTitle -ne \"\"} | ConvertTo-Csv -Delimiter '|') -replace '\"',''")
+
+print(type(processes))
+res = processes.stdout.split("\n")
+res_clean = []
+for re in res:
+	split_re = re.split("|")
+	if len(split_re) == 3 and split_re[2] != 'Id':
+		res_clean.append(split_re)
+
+print(res_clean)
