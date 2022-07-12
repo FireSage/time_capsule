0d6feb1efa1ef064c7f4699b2008de70e4b85b76
Marvin Gordon <1299703+FireSage@users.noreply.github.com>
2022-07-12 11:05:36 -0500

    Get processes function

--- src/time_capsule/time_capsule.py
+++ src/time_capsule/time_capsule.py
@@ -5,15 +5,19 @@ def run(command):
 	result = subprocess.run(["powershell", "-Command", command], capture_output=True,text=True)
 	return result
 
+def get_processes():
+	command = "(Get-Process | Select MainWindowTitle,ProcessName,Id | where{$_.MainWindowTitle -ne \"\"} | ConvertTo-Csv -Delimiter '|') -replace '\"',''"
 
-processes = run("(Get-Process | Select MainWindowTitle,ProcessName,Id | where{$_.MainWindowTitle -ne \"\"} | ConvertTo-Csv -Delimiter '|') -replace '\"',''")
+	processes = run(command)
 
-print(type(processes))
-res = processes.stdout.split("\n")
-res_clean = []
-for re in res:
-	split_re = re.split("|")
-	if len(split_re) == 3 and split_re[2] != 'Id':
-		res_clean.append(split_re)
+	# TODO: Error handling - use try block - check for error in execution
+	# print(type(processes))
+	# TODO: extract process data from string 
+	res = processes.stdout.split("\n")
+	res_clean = []
+	for re in res:
+		split_re = re.split("|")
+		if len(split_re) == 3 and split_re[2] != 'Id':
+			res_clean.append(split_re)
 
 print(res_clean)
